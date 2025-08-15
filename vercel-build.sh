0504a2f43ceacb4420766d9d83ed3ec9a186f249
Jugadveer <jugadveer278@gmail.com>
2025-08-15 21:46:31 +0530

    vercel

--- vercel-build.sh
+++ vercel-build.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+# Setup Git to use the token for authentication
+git config --global url."https://${GITHUB_TOKEN}@github.com/".insteadOf "https://github.com/"
+
+# Install and pull LFS files
 git lfs install
 git lfs pull
+
+# Run your build
 npm run build
