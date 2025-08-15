4deddc7003457a2870ebe41dc61213a2cb97911f
Jugadveer Singh <mail@example.com>
2025-08-15 22:11:10 +0530

    fix4

--- vercel-build.sh
+++ vercel-build.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 set -e
+
 echo "🔹 Checking GitHub token availability..."
 if [ -z "$GITHUB_TOKEN" ]; then
   echo "❌ GITHUB_TOKEN is missing!"
   exit 1
 fi
 
+# Auth for normal git
 git config --global url."https://${GITHUB_TOKEN}@github.com/".insteadOf "https://github.com/"
+
+# Auth for LFS
+git config --global lfs.url "https://${GITHUB_TOKEN}@github.com/Jugadveer/SwiftMedia.git/info/lfs"
+
 git lfs install --skip-repo
 git lfs fetch --all
 git lfs pull
