a4283d1dd49a90a1a919d5367b517bf87da37b9d
Jugadveer Singh <mail@example.com>
2025-08-15 22:08:57 +0530

    fix3

--- vercel-build.sh
+++ vercel-build.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 set -e
-
+echo "🔹 Checking GitHub token availability..."
 if [ -z "$GITHUB_TOKEN" ]; then
-  echo "❌ GITHUB_TOKEN not set. Please add it in Vercel Environment Variables."
+  echo "❌ GITHUB_TOKEN is missing!"
   exit 1
 fi
 
-# Configure git to use the token
 git config --global url."https://${GITHUB_TOKEN}@github.com/".insteadOf "https://github.com/"
-
-# Install and pull LFS files
 git lfs install --skip-repo
 git lfs fetch --all
 git lfs pull
